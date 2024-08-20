ba5983258105de0e82b462ae07192e99e3af9bfc
yuehui-tan <131508420+yuehui-tan@users.noreply.github.com>
2024-08-21 00:37:59 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,4 @@
 from flask import Flask, render_template, request
-import google.generativeai as palm
-import os
-
-api = os.getenv("MAKERSUITE_API_TOKEN") 
-palm.configure(api_key=api)
-model = {"model": "models/chat-bison-001"}
 
 app = Flask(__name__)
 
