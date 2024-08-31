f197e49da8928fc15dfa429e87430c87dbd6f062
Yue Hui <131508420+yuehui-tan@users.noreply.github.com>
2024-08-31 17:40:46 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,11 @@
 from flask import Flask, render_template, request
+import google.generativeai as palm 
+import os
+
+
+makersuite_api= api = os.getenv("API_KEY") 
+palm.configure(api_key = makersuite_api)
+model = {"model": "models/text-bison-001"}
 
 app = Flask(__name__)
 
@@ -6,5 +13,28 @@ app = Flask(__name__)
 def index():
     return(render_template("index.html"))
 
+
+@app.route("/ai_agent",methods=["GET","POST"])
+def ai_agent():
+    return(render_template("ai_agent.html"))
+
+@app.route("/ai_agent_reply",methods=["GET","POST"])
+def ai_agent_reply():
+    q = request.form.get("q")
+    r = palm.chat(prompt=q, **model)
+    return(render_template("ai_agent_reply.html",r=r.last))
+
+@app.route("/joke",methods=["GET","POST"])
+def joke():
+    return(render_template("joke.html"))
+
+
+@app.route("/prediction",methods=["GET","POST"])
+def prediction():
+    return(render_template("index.html"))
+
+
+
 if __name__ == "__main__":
     app.run()
+    
