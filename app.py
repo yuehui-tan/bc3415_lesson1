5bfb4bb033ef3ff363fb19ecf33806d9138347cf
Yue Hui <131508420+yuehui-tan@users.noreply.github.com>
2024-10-11 22:34:06 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,17 +1,23 @@
 from flask import Flask, render_template, request
-import google.generativeai as palm 
+import google.generativeai as genai
 import os
+from textblob import TextBlob
 
 
-makersuite_api= os.getenv("API_KEY") 
-palm.configure(api_key = makersuite_api)
-model = {"model": "models/text-bison-001"}
+#api= os.getenv("API_KEY") 
+genai.configure(api_key='AIzaSyAJiza0dAHEkFf96rN-FAaIZG7mEGBqmoc')
+model = genai.GenerativeModel("gemini-1.5-flash")
 
 app = Flask(__name__)
 
 @app.route("/",methods=["GET","POST"])
 def index():
     return(render_template("index.html"))
+
+
+# @app.route("/financial_FAQ",methods=["GET","POST"])
+# def financial_FAQ():
+#     return(render_template("financial_FAQ.html"))
 
 
 @app.route("/ai_agent",methods=["GET","POST"])
@@ -21,12 +27,24 @@ def ai_agent():
 @app.route("/ai_agent_reply",methods=["GET","POST"])
 def ai_agent_reply():
     q = request.form.get("q")
-    r = palm.chat(prompt=q, **model)
-    return(render_template("ai_agent_reply.html",r=r.last))
+    r = model.generate_content(q)
+    return(render_template("ai_agent_reply.html",r=r.text))
 
 @app.route("/joke",methods=["GET","POST"])
 def joke():
     return(render_template("joke.html"))
+
+#Sentiment Analysis
+@app.route("/SA",methods=["GET","POST"])
+def SA():
+    return(render_template("SA.html"))
+
+#Sentiment Analysis Result
+@app.route("/SAR",methods=["GET","POST"])
+def SAR():
+    q = request.form.get("q")
+    r = TextBlob(q).sentiment
+    return(render_template("SAR.html", r = r))
 
 
 @app.route("/prediction",methods=["GET","POST"])
